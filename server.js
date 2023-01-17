5b862ce77b8864ab04e724caf5656f16af4ff4d3
samgilmore <30483214+samgilmore@users.noreply.github.com>
2023-01-17 17:50:23 -0500

    Attempt to fix minimist error

--- server.js
+++ server.js
@@ -5,10 +5,8 @@ const http = require('http');
 const fs = require('fs');
 
 // Require minimist module (make sure you install this one via npm).
-const minimist = require('minimist');
-
 // Use minimist to process one argument `--port=` on the command line after `node server.js`.
-var argv = minimist(process.argv.slice(2));
+var argv = require('minimist')(process.argv.slice(2));
 
 // Define a const `port` using the argument from the command line. 
 // Make this const default to port 3000 if there is no argument given for `--port`.
